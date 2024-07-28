04e8abe998843e57d4308446ebcf501d166586cc
Arfan Anulal <151611444+ArfanAnulal@users.noreply.github.com>
2024-07-28 13:30:45 +0530

    Made it responsive for mobile screens

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,20 @@
-import { Paper,Button, Collapse, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography, Grid } from '@mui/material'
+import { Paper, Button, Collapse, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography, Grid } from '@mui/material';
 import { useMediaQuery } from '@mui/material';
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import UserDash from './UserDash'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Alert from '@mui/material/Alert';
 import CloseIcon from '@mui/icons-material/Close';
-import { Visibility, VisibilityOff } from '@mui/icons-material'
+import { Visibility, VisibilityOff } from '@mui/icons-material';
 
 const Login = () => {
-  var navigate = useNavigate()
+  const navigate = useNavigate();
 
   const isSmallScreen = useMediaQuery('(max-width:600px)');
 
   const [alertf, setAlert] = useState(false);
   const [alertContent, setAlertContent] = useState('');
-  const [alertState,setAlertState] = useState(false)
-
+  const [alertState, setAlertState] = useState(false);
 
   const [showPassword, setShowPassword] = useState(false);
   const handleClickShowPassword = () => setShowPassword((show) => !show);
@@ -24,110 +22,99 @@ const Login = () => {
     event.preventDefault();
   };
 
+  const [inputs, setInputs] = useState({ Email: '', Password: '' });
+  const inputHandler = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+    console.log(inputs);
+  };
 
-  const[inputs,setInputs] = useState({Email:'',Password:''})  
-  const inputHandler=(e)=>{
-    setInputs({...inputs,[e.target.name]:e.target.value})
-    console.log(inputs)
-  }
+  const loginHandler = () => {
+    axios.post("http://localhost:1880/login", inputs)
+      .then((res) => {
+        console.log(res);
+        setAlertContent(res.data.message);
+        setAlert(true);
+        setAlertState(true);
+        if (res.data.message === "success") {
+          alert(res.data.message);
+          if (res.data.email === 'Admin') {
+            navigate('/admin_dash');
+          } else {
+            var email = res.data.email;
+            console.log("The email is " + email);
+            localStorage.setItem('email', email); //add local storage for admin also
+            navigate('/userdash');
+          }
+        } else {
+          navigate('/login');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-
-  const loginHandler=()=>{
-     axios.post("http://localhost:1880/login",inputs)
-        .then((res)=>{
-            console.log(res);
-            setAlertContent(res.data.message);
-            setAlert(true);
-            setAlertState(true);
-            if (res.data.message == "success" ){
-              alert(res.data.message)
-                  if   (res.data.email=='Admin') {
-                      navigate('/admin_dash')
-                    } 
-
-                  else{
-                    var email = res.data.email
-                    console.log("The email is "+email);
-                    localStorage.setItem('email',email) //add local storage for admin also
-                    navigate('/userdash')
-                      }
-                }
-
-            else{
-              navigate('/login')
-            }
-            
-        })
-        .catch((err)=>{
-          console.log(err)
-        })
-  }
-    
   return (
-    <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
-      <Grid item xs={10} sm={8} md={6} lg={4} xl={3} style={{ textAlign: 'center' }}>
-      <Paper sx={{backgroundColor:'#dcdcdc', marginLeft:'5%',marginRight:'5%'}} elevation={15} >
-        <Collapse in={alertState}>
-          {alertf && (
-            <Alert
-              action={
-                <IconButton onClick={() => setAlertState(false)} style={{ marginBottom: '-10%' }}>
-                  <CloseIcon />
-                </IconButton>
-              }
-              severity="error"
-            >
-              {alertContent}
-            </Alert>
-          )}
-        </Collapse>
-        <br /><br />
-        <Typography variant="h3" style={{ fontFamily: 'times', paddingTop:'2%' }}>Login</Typography>
-        <br /><br />
-        <TextField
-          variant="outlined"
-          label="Email"
-          onChange={inputHandler}
-          name="Email"
-          value={inputs.Email}
-          fullWidth
-          sx={{ m: 1, width: isSmallScreen ? '100%' : '30ch' }}
-        />
-        <br /><br />
-        <FormControl fullWidth sx={{ m: 1, width: isSmallScreen ? '100%' : '30ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            name="Password"
+    <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh', padding: isSmallScreen ? '1rem' : '2rem' }}>
+      <Grid item xs={12} sm={10} md={8} lg={6} xl={4} style={{ textAlign: 'center' }}>
+        <Paper sx={{ backgroundColor: '#dcdcdc', padding: '2rem' }} elevation={15}>
+          <Collapse in={alertState}>
+            {alertf && (
+              <Alert
+                action={
+                  <IconButton onClick={() => setAlertState(false)}>
+                    <CloseIcon />
+                  </IconButton>
+                }
+                severity="error"
+              >
+                {alertContent}
+              </Alert>
+            )}
+          </Collapse>
+          <Typography variant={isSmallScreen ? 'h4' : 'h3'} style={{ fontFamily: 'times', paddingTop: '1rem' }}>Login</Typography>
+          <TextField
+            variant="outlined"
+            label="Email"
             onChange={inputHandler}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
+            name="Email"
+            value={inputs.Email}
+            fullWidth
+            sx={{ m: 2 }}
           />
-        </FormControl>
-        <br /><br />
-        <Button
-          variant="contained"
-          onClick={loginHandler}
-          style={{ backgroundColor: '#183e4b', fontFamily: 'times', borderRadius: '2rem',marginBottom:'5%'  }}
-        >
-          Log In
-        </Button>
+          <FormControl fullWidth sx={{ m: 2 }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              name="Password"
+              onChange={inputHandler}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <Button
+            variant="contained"
+            onClick={loginHandler}
+            style={{ backgroundColor: '#183e4b', fontFamily: 'times', borderRadius: '2rem', marginBottom: '1rem', width: '30%', padding: '0.75rem', fontSize: '1rem' }}
+          >
+            Log In
+          </Button>
         </Paper>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,13 +1,13 @@
-import { Visibility, VisibilityOff } from '@mui/icons-material'
-import { Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField, Typography } from '@mui/material';
 import { useMediaQuery } from '@mui/material';
 import Paper from '@mui/material/Paper';
-import axios from 'axios'
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SignUp = () => {
-  var navigate = useNavigate()
+  const navigate = useNavigate();
 
   const isSmallScreen = useMediaQuery('(max-width:600px)');
   const isMediumScreen = useMediaQuery('(min-width:600px) and (max-width:960px)');
@@ -21,135 +21,139 @@ const SignUp = () => {
     event.preventDefault();
   };
 
+  const [inputs, setInputs] = useState({ Username: '', Email: '', Password: '', CPassword: '' });
 
-  const[inputs,setInputs] = useState({Username:'',Email:'',Password:'',CPassword:""})
-  
-  const inputHandler=(e)=>{
-    setInputs({...inputs,[e.target.name]:e.target.value})
-    console.log(inputs)
-  }
-  
+  const inputHandler = (e) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+    console.log(inputs);
+  };
 
-  const signupHandler=()=>{
-    if (inputs.Password==inputs.CPassword){
-      const finalinputs = {Username:inputs.Username,Email:inputs.Email,Password:inputs.Password}
-      console.log(finalinputs)
-    axios.post("http://localhost:1880/add_user",finalinputs)
-        .then((res)=>{
-            console.log(res)
-            if (res.data.message=='This email already exists'){
-            alert(res.data.message)
-            navigate('/signup')
-            }
-            else{
-              alert(res.data.message)
-              navigate('/')
-            }
+  const signupHandler = () => {
+    if (inputs.Password === inputs.CPassword) {
+      const finalinputs = { Username: inputs.Username, Email: inputs.Email, Password: inputs.Password };
+      console.log(finalinputs);
+      axios.post("http://localhost:1880/add_user", finalinputs)
+        .then((res) => {
+          console.log(res);
+          if (res.data.message === 'This email already exists') {
+            alert(res.data.message);
+            navigate('/signup');
+          } else {
+            alert(res.data.message);
+            navigate('/');
+          }
         })
-        .catch((err)=>{
-          console.log(err)
-        })
-      }
-      else{
-        alert('Passwords do not match, try again')
-      }
-  }
-
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      alert('Passwords do not match, try again');
+    }
+  };
 
   return (
-    
-    <div style={{ 
-      display: 'flex', 
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      height: '100vh', 
-      padding: '0 1rem',
-      overflow: isMediumScreen || isLargeScreen ? 'hidden' : 'auto'
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      padding: '1rem',
     }}>
-      <div style={{ 
-        textAlign: 'center', 
-        width: isSmallScreen ? '100%' : '50%', 
-        
+      <div style={{
+        textAlign: 'center',
+        width: isSmallScreen ? '100%' : isMediumScreen ? '75%' : '50%',
+        maxWidth: '500px',
       }}>
-        <Paper sx={{backgroundColor:'#dcdcdc', marginLeft:'25%',marginRight:'25%',marginTop:'4%'}} elevation={15} >
-        <Typography variant='h3' style={{ fontFamily: 'times', visibility: 'visible', paddingTop:'2%' }}>Sign Up</Typography>
-        <br /><br />
-        <TextField
-          variant='outlined'
-          label='Username'
-          onChange={inputHandler}
-          name='Username'
-          value={inputs.Username}
-          sx={{ m: 1, width: isSmallScreen ? '100%' : '30ch' }}
-        />
-        <br /><br />
-        <TextField
-          variant='outlined'
-          label='Email'
-          onChange={inputHandler}
-          name='Email'
-          value={inputs.Email}
-          sx={{ m: 1, width: isSmallScreen ? '100%' : '30ch' }}
-        />
-        <br /><br />
-        <FormControl sx={{ m: 1, width: isSmallScreen ? '100%' : '30ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            name='Password'
+        <Paper sx={{
+          backgroundColor: '#dcdcdc',
+          margin: '2rem 1rem',
+          padding: '2rem',
+        }} elevation={15}>
+          <Typography variant={isSmallScreen ? 'h4' : 'h3'} style={{ fontFamily: 'times', visibility: 'visible' }}>Sign Up</Typography>
+          <br />
+          <TextField
+            variant='outlined'
+            label='Username'
             onChange={inputHandler}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
+            name='Username'
+            value={inputs.Username}
+            sx={{ m: 1, width: '100%' }}
           />
-        </FormControl>
-        <br /><br />
-        <FormControl sx={{ m: 1, width: isSmallScreen ? '100%' : '30ch' }} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-confirmpassword">Confirm Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-confirmpassword"
-            type={showPassword ? 'text' : 'password'}
-            name='CPassword'
+          <br />
+          <TextField
+            variant='outlined'
+            label='Email'
             onChange={inputHandler}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {showPassword ? <VisibilityOff /> : <Visibility />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Confirm Password"
+            name='Email'
+            value={inputs.Email}
+            sx={{ m: 1, width: '100%' }}
           />
-        </FormControl>
-        <br /><br />
-        <Button
-          variant='contained'
-          onClick={signupHandler}
-          style={{ backgroundColor: '#183e4b', fontFamily: 'times', borderRadius: '2rem',marginBottom:'5%' }}
-        >
-          Sign Up
-        </Button>
+          <br />
+          <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              name='Password'
+              onChange={inputHandler}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <br />
+          <FormControl sx={{ m: 1, width: '100%' }} variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-confirmpassword">Confirm Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-confirmpassword"
+              type={showPassword ? 'text' : 'password'}
+              name='CPassword'
+              onChange={inputHandler}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Confirm Password"
+            />
+          </FormControl>
+          <br />
+          <Button
+            variant='contained'
+            onClick={signupHandler}
+            style={{
+              backgroundColor: '#183e4b',
+              fontFamily: 'times',
+              borderRadius: '2rem',
+              marginBottom: '5%',
+              width: '50%',
+              padding: isSmallScreen ? '0.5rem' : '0.75rem',
+              fontSize: isSmallScreen ? '0.875rem' : '1rem'
+            }}
+          >
+            Sign Up
+          </Button>
         </Paper>
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
